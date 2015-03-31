23287979a701d8eac52dd9f791fe859fabb1fdca
rizki <rizki_syarif@brown.edu>
2015-03-30 19:01:36 -0500

    added funtion for comparing higgs mass window

--- Plot_efficiency.C
+++ Plot_efficiency.C
@@ -451,3 +451,278 @@ void Plot_efficiency(){
   c->SaveAs((dir+"/"+"ROC.eps").c_str());
 
 }
+
+void Plot_efficiency_dMHigs10_dMHigs20(){
+  string dir = "allChi_noMinFatjetPt_noMjBtagCondition";
+  string deltaHiggsMass;
+  string fsig;  string fsig2;
+  string fbkg;  string fbkg2;
+  string postfix = "dRmax12_NoGenPtCut_NoFjPtCut_AllChi";
+  double xbin = 40; double xmin = -18; double xmax = -2;
+  bool save = false;
+  string var = "log(Fj_chi)" ; string xlabel = "Log(#chi)";
+  string cut = "Fj_chi>0";
+  string loose_cut = cut;
+  string medium_cut; if(cut==""){medium_cut = "dR_match<0.2";} else {medium_cut = ("dR_match<0.2&&"+cut).c_str();}  ;
+  string tight_cut; if(cut==""){tight_cut = "dR_match<0.05";} else {tight_cut = ("dR_match<0.05&&"+cut).c_str();}  ;
+  Color_t color1= kBlue;
+  Color_t color2 = kRed;
+
+  deltaHiggsMass = "HiggsWin10";
+  fsig = "RadionToHH_4b_M-800_TuneZ2star_8TeV-Madgraph_pythia6_R12_r15_minPt0_Nobtagmjcondition_AllChi_"+deltaHiggsMass+"_mc_subjets";
+  fbkg = "ZPrimeToTTJets_M1000GeV_W10GeV_TuneZ2star_8TeV-madgraph-tauola_R12_r15_minPt0_Nobtagmjcondition_AllChi_"+deltaHiggsMass+"_mc_subjets";
+  TFile *fs = new TFile((dir+"/"+"fMatch_"+fsig+"_"+postfix+".root").c_str());
+  TFile *fb = new TFile((dir+"/"+"fMatch_"+fbkg+"_"+postfix+".root").c_str());
+
+  TH1D* h_sig_t = PartonMatch_1D(dir,fs,fsig,var,tight_cut,xbin,xmin,xmax,xlabel,postfix,color1,1,save);
+  TH1D* h_bkg_t = PartonMatch_1D(dir,fb,fbkg,var,tight_cut,xbin,xmin,xmax,xlabel,postfix,color2,1,save);
+
+  deltaHiggsMass = "HiggsWin20";
+  fsig2 = "RadionToHH_4b_M-800_TuneZ2star_8TeV-Madgraph_pythia6_R12_r15_minPt0_Nobtagmjcondition_AllChi_"+deltaHiggsMass+"_mc_subjets";
+  fbkg2 = "ZPrimeToTTJets_M1000GeV_W10GeV_TuneZ2star_8TeV-madgraph-tauola_R12_r15_minPt0_Nobtagmjcondition_AllChi_"+deltaHiggsMass+"_mc_subjets";
+  TFile *fs2 = new TFile((dir+"/"+"fMatch_"+fsig2+"_"+postfix+".root").c_str());
+  TFile *fb2 = new TFile((dir+"/"+"fMatch_"+fbkg2+"_"+postfix+".root").c_str());
+
+  TH1D* h_sig_t2 = PartonMatch_1D(dir,fs2,fsig2,var,tight_cut,xbin,xmin,xmax,xlabel,postfix,color1,2,save);
+  TH1D* h_bkg_t2 = PartonMatch_1D(dir,fb2,fbkg2,var,tight_cut,xbin,xmin,xmax,xlabel,postfix,color2,2,save);
+
+
+  //---------Calculating efficiciencies----------
+
+  int sig_t_nbins = h_sig_t->GetSize()-2;
+  int bkg_t_nbins = h_bkg_t->GetSize()-2;
+  double Ssig_t = h_sig_t->Integral(0,sig_t_nbins+1);
+  double Sbkg_t = h_bkg_t->Integral(0,bkg_t_nbins+1);
+  double sig_t_integral[sig_t_nbins];
+  double bkg_t_integral[bkg_t_nbins];
+  double sig_t_eff[sig_t_nbins];
+  double bkg_t_eff[bkg_t_nbins];
+  double x_t[sig_t_nbins]; //double xmin = -18.; double xmax = -2.;
+  double increment_t = (xmax-xmin)/sig_t_nbins;
+  cout<<"---tight---"<<endl;
+  for(int i=0;i<sig_t_nbins;i++){
+    sig_t_integral[i]=h_sig_t->Integral(i+1,sig_t_nbins+1);
+    bkg_t_integral[i]=h_bkg_t->Integral(i+1,bkg_t_nbins+1);
+    sig_t_eff[i] = sig_t_integral[i]/Ssig_t;
+    bkg_t_eff[i] = bkg_t_integral[i]/Sbkg_t;
+    x_t[i] = xmin + increment_t*i;
+    cout<<i+1<<". x = "<<x_t[i]<< ", sig_t =  "<< sig_t_integral[i]/Ssig_t<< ", bkg_t = "<< bkg_t_integral[i]/Sbkg_t << endl;
+  }
+  cout << endl;
+
+  int sig_t2_nbins = h_sig_t2->GetSize()-2;
+  int bkg_t2_nbins = h_bkg_t2->GetSize()-2;
+  double Ssig_t2 = h_sig_t2->Integral(0,sig_t2_nbins+1);
+  double Sbkg_t2 = h_bkg_t2->Integral(0,bkg_t2_nbins+1);
+  double sig_t2_integral[sig_t2_nbins];
+  double bkg_t2_integral[bkg_t2_nbins];
+  double sig_t2_eff[sig_t2_nbins];
+  double bkg_t2_eff[bkg_t2_nbins];
+  double x_t2[sig_t2_nbins]; //double xmin = -18.; double xmax = -2.;
+  double increment_t2 = (xmax-xmin)/sig_t2_nbins;
+  cout<<"---tight--- dMHigs 20"<<endl;
+  for(int i=0;i<sig_t2_nbins;i++){
+    sig_t2_integral[i]=h_sig_t2->Integral(i+1,sig_t2_nbins+1);
+    bkg_t2_integral[i]=h_bkg_t2->Integral(i+1,bkg_t2_nbins+1);
+    sig_t2_eff[i] = sig_t2_integral[i]/Ssig_t2;
+    bkg_t2_eff[i] = bkg_t2_integral[i]/Sbkg_t2;
+    x_t2[i] = xmin + increment_t2*i;
+    cout<<i+1<<". x = "<<x_t2[i]<< ", sig_t2 =  "<< sig_t2_integral[i]/Ssig_t2<< ", bkg_t2 = "<< bkg_t2_integral[i]/Sbkg_t2 << endl;
+  }
+  cout << endl;
+
+  //---------------tight------------------------
+
+  TCanvas *c3_t = new TCanvas("c3_t","c3_t",800, 600);
+
+  TMultiGraph *mg_t = new TMultiGraph("mg_t","mg_t");
+
+  TGraph *gr1_t = new TGraph(sig_t_nbins,x_t,sig_t_eff);
+  gr1_t->SetName("gr1_t");
+  gr1_t->SetTitle("RadionHH");
+  gr1_t->SetMarkerStyle(21);
+  gr1_t->SetMarkerColor(kGreen);
+  gr1_t->SetDrawOption("AP");
+  gr1_t->SetLineColor(kBlue);
+  gr1_t->SetLineWidth(2);
+  gr1_t->SetFillStyle(0);
+
+  TGraph *gr2_t = new TGraph(sig_t_nbins,x_t,bkg_t_eff);
+  gr2_t->SetName("gr2_t");
+  gr2_t->SetTitle("ttjets");
+  gr2_t->SetMarkerStyle(22);
+  gr2_t->SetMarkerColor(kRed+4);
+  gr2_t->SetDrawOption("P");
+  gr2_t->SetLineColor(kRed);
+  gr2_t->SetLineWidth(2);
+  gr2_t->SetFillStyle(0);
+
+  TGraph *gr1_t2 = new TGraph(sig_t2_nbins,x_t2,sig_t2_eff);
+  gr1_t2->SetName("gr1_t2");
+  gr1_t2->SetTitle("RadionHH");
+  gr1_t2->SetMarkerStyle(21);
+  gr1_t2->SetMarkerColor(kGreen);
+  gr1_t2->SetDrawOption("AP");
+  gr1_t2->SetLineColor(kBlue);
+  gr1_t2->SetLineWidth(2);
+  gr1_t2->SetFillStyle(0);
+
+  TGraph *gr2_t2 = new TGraph(sig_t2_nbins,x_t2,bkg_t2_eff);
+  gr2_t2->SetName("gr2_t2");
+  gr2_t2->SetTitle("ttjets");
+  gr2_t2->SetMarkerStyle(22);
+  gr2_t2->SetMarkerColor(kRed+4);
+  gr2_t2->SetDrawOption("P");
+  gr2_t2->SetLineColor(kRed);
+  gr2_t2->SetLineWidth(2);
+  gr2_t2->SetFillStyle(0);
+
+  mg_t->Add( gr1_t );
+  mg_t->Add( gr2_t );
+  mg_t->Add( gr1_t2 );
+  mg_t->Add( gr2_t2 );
+
+  mg_t->Draw("APL");
+  mg_t->GetXaxis()->SetTitle("log(#chi) >");
+  mg_t->GetYaxis()->SetTitle("Efficiency");
+  mg_t->SetTitle("after loose matching");
+  c3_t->BuildLegend(0.7,.75,.9,.9);
+
+  c3_t->SaveAs((dir+"/"+"efficiency_tight_deltaHiggsMass.eps").c_str());
+
+  //---------superimpose signal efficiency graph------
+
+  TCanvas *c_sig = new TCanvas("c_sig","c_sig",800,600);
+  c_sig->cd();
+
+  TMultiGraph *mg_sig = new TMultiGraph("sig","sig");
+
+  mg_sig->Add(gr1_t);
+  mg_sig->Add(gr1_t2);
+
+  gr1_t->SetMarkerStyle(7);
+  gr1_t2->SetMarkerStyle(4);
+
+  mg_sig->Draw("APL");
+  mg_sig->GetXaxis()->SetTitle("log(#chi) >");
+  mg_sig->GetYaxis()->SetTitle("Efficiency");
+  mg_sig->SetTitle("after loose matching");
+  //c_sig->BuildLegend(0.7,.75,.9,.9);
+
+  c_sig->SaveAs((dir+"/"+"efficiency_sig_deltaHiggsMass.eps").c_str());
+
+  //---------superimpose bkg efficiency graph------
+
+  TCanvas *c_bkg = new TCanvas("c_bkg","c_bkg",800,600);
+  c_bkg->cd();
+
+  TMultiGraph *mg_bkg = new TMultiGraph("bkg","bkg");
+
+  mg_bkg->Add(gr2_t);
+  mg_bkg->Add(gr2_t2);
+
+  gr2_t->SetMarkerStyle(7);
+  gr2_t2->SetMarkerStyle(4);
+
+  mg_bkg->Draw("APL");
+  mg_bkg->GetXaxis()->SetTitle("log(#chi) >");
+  mg_bkg->GetYaxis()->SetTitle("Efficiency");
+  mg_bkg->SetTitle("after loose matching");
+  //c_bkg->BuildLegend(0.7,.75,.9,.9);
+
+  c_bkg->SaveAs((dir+"/"+"efficiency_bkg_deltaHigssMass.eps").c_str());
+
+  //---------superimpose sig&bkg efficiency graph------
+
+  TCanvas *c_sb = new TCanvas("c_sb","c_sb",800,600);
+  c_sb->cd();
+
+  TMultiGraph *mg_sb = new TMultiGraph("sb","sb");
+
+  gr1_t->SetTitle("HH - dR<0.05");
+  gr2_t->SetTitle("tt - dR<0.05");
+  gr1_t2->SetTitle("HH - dR<0.05");
+  gr2_t2->SetTitle("tt - dR<0.05");
+
+  mg_sb->Add(gr1_t);
+  mg_sb->Add(gr2_t);
+  mg_sb->Add(gr1_t2);
+  mg_sb->Add(gr2_t2);
+
+  mg_sb->Draw("APL");
+  mg_sb->GetXaxis()->SetTitle("log(#chi) >");
+  mg_sb->GetYaxis()->SetTitle("Efficiency");
+  mg_sb->SetTitle("after loose matching");
+  c_sb->BuildLegend(0.7,.65,.9,.9);
+
+  c_sb->SaveAs((dir+"/"+"efficiency_sb_deltaHiggsMass.eps").c_str());
+
+  //------------ROC-------------------------
+
+  TCanvas *c = new TCanvas("c","c",800,600);
+  c->cd();
+
+  TMultiGraph *mg_ROC = new TMultiGraph("ROC","ROC");
+
+
+  double bkg_t_rej[bkg_t_nbins];
+  for(int i =0;i<bkg_t_nbins;i++)bkg_t_rej[i]=1-bkg_t_eff[i];
+
+  TGraph *gr3_t = new TGraph(sig_t_nbins,sig_t_eff,bkg_t_rej);
+  gr3_t->SetTitle("tight");
+  gr3_t->SetLineColor(kBlue);
+  gr3_t->SetLineWidth(2);
+  gr3_t->GetXaxis()->SetTitle("Eff_{sig}");
+  gr3_t->GetYaxis()->SetTitle("1-Eff_{bkg}");
+  gr3_t->GetXaxis()->SetRangeUser(0,1);
+  gr3_t->GetYaxis()->SetRangeUser(0,1);
+  gr3_t->SetFillStyle(0);
+
+  double bkg_t2_rej[bkg_t2_nbins];
+  for(int i =0;i<bkg_t2_nbins;i++)bkg_t2_rej[i]=1-bkg_t2_eff[i];
+
+  TGraph *gr3_t2 = new TGraph(sig_t2_nbins,sig_t2_eff,bkg_t2_rej);
+  gr3_t2->SetTitle("tight");
+  gr3_t2->SetLineColor(kBlue);
+  gr3_t2->SetLineWidth(2);
+  gr3_t2->GetXaxis()->SetTitle("Eff_{sig}");
+  gr3_t2->GetYaxis()->SetTitle("1-Eff_{bkg}");
+  gr3_t2->GetXaxis()->SetRangeUser(0,1);
+  gr3_t2->GetYaxis()->SetRangeUser(0,1);
+  gr3_t2->SetFillStyle(0);
+
+  mg_ROC->Add(gr3_t);
+  mg_ROC->Add(gr3_t2);
+
+  mg_ROC->Draw("APL");
+  mg_ROC->GetXaxis()->SetTitle("Eff_{sig}");
+  mg_ROC->GetYaxis()->SetTitle("1-Eff_{bkg}");
+  mg_ROC->GetXaxis()->SetRangeUser(0,1);
+  mg_ROC->GetYaxis()->SetRangeUser(0,1);
+  mg_ROC->SetTitle("ROC Curves");
+
+
+  c->BuildLegend(0.2,0.2,0.2+0.2,0.2+0.15);
+
+  //-----putting marker on graph-----
+
+  /*
+  int pt_i = 18; //(10,17,25)
+  double pt = xmin+increment*pt_i;
+  ostringstream ostr;
+  ostr << pt;
+  string pt_s = ostr.str();
+
+  TLatex *latex_t = new TLatex(gr3_t->GetX()[pt_i], gr3_t->GetY()[pt_i],pt_s.c_str());
+  gr3_t->GetListOfFunctions()->Add(latex_t);
+  latex_t->SetTextSize(0.02);
+  TMarker* marker_t = new TMarker;
+  marker_t->SetMarkerStyle(26);
+  marker_t->SetMarkerSize(0.5);
+  marker_t->DrawMarker(gr3_t->GetX()[pt_i], gr3_t->GetY()[pt_i]);
+  marker_t->Draw("SAME");
+  */
+
+  c->SaveAs((dir+"/"+"ROC_deltaHiggsMass.eps").c_str());
+
+}
