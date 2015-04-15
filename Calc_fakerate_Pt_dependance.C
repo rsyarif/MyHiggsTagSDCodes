8f6c4a1b49db8db9be9302f628084682a7c6131f
rizki <rizki_syarif@brown.edu>
2015-04-15 14:49:08 -0500

    for investigating fakerate pt dependence of IVFmatched (btag==1) Microjets in QCD sample

---
+++ Calc_fakerate_Pt_dependance.C
@@ -0,0 +1,78 @@
+{
+
+  TFile *f = new TFile("rootfiles/FakeMjIVFbtagCalculation_QCD_Pt-470to600_Tune4C_13TeV_pythia8_R08_r015_HiggsWin20_mc_subjets.root");
+  TTree * t= f->Get("tree");
+
+  int nbins = 40; float xmin = 0; float xmax = 800;
+
+  TH1D *h1 = new TH1D("h1","h1",nbins,xmin,xmax);
+  TH1D *h2 = new TH1D("h2","h2",nbins,xmin,xmax);
+
+  gStyle->SetOptStat("nemrou");
+
+  h1->SetLineColor(kBlue);
+  h1->SetLineWidth(2);
+  h1->SetTitle("abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
+  h1->GetXaxis()->SetTitle("Microjet p_{T}");
+
+  h2->SetLineColor(kBlue);
+  h2->SetLineWidth(2);
+  h2->SetTitle("abs(Mj_gen_matched_pdgID)<99999.eps&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1_.eps");
+  h2->GetXaxis()->SetTitle("Microjet p_{T}");
+
+  t->Draw("Mj_pt>>h1","abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
+
+  c1->SaveAs("rootfiles/QCD470-600_Mj_pt_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps");
+
+  t->Draw("Mj_pt>>h2","Mj_isBtag==1&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
+
+  c1->SaveAs("rootfiles/QCD470-600_Mj_pt_Mj_isBtag==1&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps");
+
+
+  //t->Draw("Mj_pt>>h1","abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
+
+  //c1->SaveAs("rootfiles/QCD470-600_Mj_pt_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4_.eps");
+
+  //t->Draw("Mj_pt>>h2","abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1");
+
+  //c1->SaveAs("rootfiles/QCD470-600_Mj_pt_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1_.eps");
+
+  //t->Draw("Mj_pt>>h1","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
+
+  //c1->SaveAs("rootfiles/QCD470-600_Mj_pt_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4_.eps");
+
+  //t->Draw("Mj_pt>>h2","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1");
+
+  //c1->SaveAs("rootfiles/QCD470-600_Mj_pt_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1_.eps");
+
+
+  //int nbins  = h1->GetSize()-2;
+
+  float v1[nbins];
+  float v1_std[nbins];
+  float v2[nbins];
+  float v2_std[nbins];
+  float x[nbins];
+  float ratio[nbins];
+  float ratio_err_x[nbins];
+  float ratio_err_y[nbins];
+
+  for (int i =0 ; i<nbins;i++){
+    v1[i] = h1->GetBinContent(i+1);
+    v1_std[i] = sqrt(v1[i]);
+    v2[i] = h2->GetBinContent(i+1);
+    v2_std[i] = sqrt(v2[i]);
+    ratio[i] = (v2[i]==0? 0 : v2[i]/v1[i]);
+    ratio_err_x[i] = (xmax - xmin)/nbins *0.5;
+    ratio_err_y[i] = ratio[i] * sqrt( (v1_std[i] / v1[i])**2  + (v2_std[i] / v2[i])**2 ) ;
+    x[i] = xmin + i * (xmax-xmin)/nbins ;
+  }
+
+  TGraphErrors *gr = new TGraphErrors(nbins,x,ratio,ratio_err_x,ratio_err_y);
+  gr->Draw("AP");
+  gr->SetTitle("ratio of #frac{IVF matched non-b/c Mj}{non-b/c Mj}");
+  gr->GetXaxis()->SetTitle("p_{T}");
+
+  c1->SetGridy();
+  c1->SaveAs("rootfiles/QCD_Pt-470to600_IVFfakerate_PtDependance.eps");
+}
