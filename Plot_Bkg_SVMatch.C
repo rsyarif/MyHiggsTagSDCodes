25eb7f27ae6bc45ad48374c2ae29980efd360020
rizki <rizki_syarif@brown.edu>
2015-05-05 16:20:51 -0500

    modified pr dependence calculation. overlaps with calc_fakerate macro

--- Plot_Bkg_SVMatch.C
+++ Plot_Bkg_SVMatch.C
@@ -1,139 +1,143 @@
 {
   string prefix = "FakeMjIVFbtagCalculation_";
+
   //string fname = "RelValProdTTbar_13_AOD";
-  //string fname = "QCD_Pt-470to600_Tune4C_13TeV_pythia8";
-  string fname = "QCD_Pt-300to470_Tune4C_13TeV_pythia8";
+  string fname = "QCD_Pt-470to600_Tune4C_13TeV_pythia8";
+  //string fname = "QCD_Pt-300to470_Tune4C_13TeV_pythia8";
+
+  //string type = "_R08_r015_HiggsWin20_tagr07_fake02_BtagALLMj_MjIVF015_mc_subjets"; string postfix ="";
+  string type = "_R08_r015_AkMj_HiggsWin30_tagr07_fake02_BtagALLMj_MjIVF015_mc_subjets"; string postfix = "_AkMj"; //DOUBLE CHECK postfix! for saving name purposes.
 
   TCanvas *c1 = new TCanvas();
 
   gStyle->SetOptStat("nemrou");
 
-  TFile *f = new TFile(("rootfiles/"+prefix+fname+"_R08_r015_HiggsWin20_tagr07_fake02_BtagALLMj_MjIVF015_mc_subjets.root").c_str());
+  TFile *f = new TFile(("rootfiles/"+prefix+fname+type+".root").c_str());
   TTree *t = (TTree*) f->Get("tree");
 
   t->Draw("Mj_pt","abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4.eps").c_str());
 
   t->Draw("Mj_pt","abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1.eps").c_str());
   //
   t->Draw("Mj_eta","abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_eta_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_eta_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4.eps").c_str());
 
   t->Draw("Mj_eta","abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_eta_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_eta_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1.eps").c_str());
   //
   t->Draw("Mj_gen_matched_pdgID","abs(Mj_dR_gen)<99999&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_pdgID_abs(Mj_dR_gen)<99999&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_pdgID_abs(Mj_dR_gen)<99999&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4.eps").c_str());
 
   t->Draw("Mj_gen_matched_pdgID","abs(Mj_dR_gen)<99999&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_pdgID_abs(Mj_dR_gen)<99999&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_pdgID_abs(Mj_dR_gen)<99999&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1.eps").c_str());
   //
   t->Draw("Fj_flavour","abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Fj_flavour_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Fj_flavour_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4.eps").c_str());
 
   t->Draw("Fj_flavour","abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Fj_flavour_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Fj_flavour_abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4&&Mj_isBtag==1.eps").c_str());
   //
   t->Draw("Mj_dR_SV","Mj_dR_SV>=0&&Mj_isBtag==1&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_dR_SV_Mj_dR_SV>=0&&Mj_isBtag==1&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_dR_SV_Mj_dR_SV>=0&&Mj_isBtag==1&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps").c_str());
 
   t->Draw("Mj_dR_SV","Mj_dR_SV>=0&&Mj_dR_SV<0.15&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_dR_SV_Mj_dR_SV>=0&&Mj_dR_SV<0.15&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_dR_SV_Mj_dR_SV>=0&&Mj_dR_SV<0.15&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps").c_str());
   //
   t->Draw("Mj_dR_SV_1t1","Mj_dR_SV_1t1>=0&&Mj_dR_SV_1t1<0.15&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_dR_SV_1t1_Mj_dR_SV_1t1>=0&&Mj_dR_SV_1t1<0.15&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_dR_SV_1t1_Mj_dR_SV_1t1>=0&&Mj_dR_SV_1t1<0.15&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps").c_str());
 
   t->Draw("Mj_dR_SV_1t1","Mj_dR_SV_1t1>=0&&Mj_dR_SV_1t1<0.015&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_dR_SV_1t1_Mj_dR_SV_1t1>=0&&Mj_dR_SV_1t1<0.015&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_dR_SV_1t1_Mj_dR_SV_1t1>=0&&Mj_dR_SV_1t1<0.015&&abs(Fj_flavour)!=5&&abs(Fj_flavour)!=4_.eps").c_str());
   //
   t->Draw("Mj_dR_gen","abs(Mj_gen_matched_pdgID)<99999");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_dR_gen_abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_dR_gen_abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
   //
   t->Draw("Mj_pt","abs(Mj_gen_matched_pdgID)<99999");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
 
   t->Draw("Mj_pt","abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
 
   t->Draw("Mj_pt","abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
 
   t->Draw("Mj_pt","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
 
   t->Draw("Mj_pt","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
 
   t->Draw("Mj_pt","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
   //
   t->Draw("Mj_gen_matched_pdgID","abs(Mj_gen_matched_pdgID)<99999");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_pdgID_abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_pdgID_abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
 
   t->Draw("Mj_gen_matched_pdgID","abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_pdgID_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_pdgID_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
 
   t->Draw("Mj_gen_matched_pdgID","abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_pdgID_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_pdgID_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
 
   t->Draw("Mj_gen_matched_pdgID","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_pdgID_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_pdgID_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
 
   t->Draw("Mj_gen_matched_pdgID","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_pdgID_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_pdgID_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
 
   t->Draw("Mj_gen_matched_pdgID","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_pdgID_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_pdgID_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
   //
   t->Draw("Mj_gen_matched_eta","abs(Mj_gen_matched_pdgID)<99999");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_eta_abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_eta_abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
 
   t->Draw("Mj_gen_matched_eta","abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_eta_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_eta_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
 
   t->Draw("Mj_gen_matched_eta","abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_eta_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_eta_abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
 
   t->Draw("Mj_gen_matched_eta","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_eta_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_eta_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999.eps").c_str());
 
   t->Draw("Mj_gen_matched_eta","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_eta_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_eta_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
 
   t->Draw("Mj_gen_matched_eta","Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_gen_matched_eta_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_gen_matched_eta_Mj_dR_gen<0.05&&abs(Mj_gen_matched_pdgID)<99999&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4&&Mj_isBtag==1.eps").c_str());
   //
   TH2D *h_1 = new TH2D("h_1","h_1",50,0,1000,51,-25.5,25.5);
   TH2D *h_2 = new TH2D("h_2","h_2",50,0,1000,51,-25.5,25.5);
@@ -141,18 +145,18 @@
   t->Draw("Mj_gen_matched_pdgID:Mj_pt>>h_2","Mj_isBtag==1&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4");
 
   h_1->Draw("LEGO2");
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt:Mj_gen_matched_pdgID_abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt:Mj_gen_matched_pdgID_abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
 
   h_2->Draw("LEGO2");
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt:Mj_gen_matched_pdgID_Mj_isBtag==1&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt:Mj_gen_matched_pdgID_Mj_isBtag==1&&abs(Mj_gen_matched_pdgID)!=5&&abs(Mj_gen_matched_pdgID)!=4.eps").c_str());
 
   //
 
-  TFile *f_ = new TFile(("rootfiles/"+fname+"_R08_r015_HiggsWin20_tagr07_fake02_BtagALLMj_MjIVF015_mc_subjets.root").c_str());
+  TFile *f_ = new TFile(("rootfiles/"+fname+type+".root").c_str());
   TDirectory *d_ = f_->GetDirectory("btaganaSubJets");
   TTree *t_ = (TTree*) d_->Get("ttree");
 
-  const int nbins = 50;float xmin = 0;float xmax = 800;
+  const int nbins = 40;float xmin = 0;float xmax = 800;
 
   TH1D *h1 = new TH1D("h1","h1",nbins,xmin,xmax);
   TH1D *h2 = new TH1D("h2","h2",nbins,xmin,xmax);
@@ -271,50 +275,80 @@
   h1->Draw();
   h1->GetXaxis()->SetTitle("Mj p_{T}");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_FatJetInfo.nJet>0&&abs(FatJetInfo.Jet_flavour)!=5&&abs(FatJetInfo.Jet_flavour)!=4_LOOP.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_FatJetInfo.nJet>0&&abs(FatJetInfo.Jet_flavour)!=5&&abs(FatJetInfo.Jet_flavour)!=4_LOOP.eps").c_str());
 
   h2->Draw();
   h2->GetXaxis()->SetTitle("Mj p_{T}");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Mj_pt_FatJetInfo.nJet>0&&abs(FatJetInfo.Jet_flavour)!=5&&abs(FatJetInfo.Jet_flavour)!=4&&Mj_isBtag==1_LOOP.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Mj_pt_FatJetInfo.nJet>0&&abs(FatJetInfo.Jet_flavour)!=5&&abs(FatJetInfo.Jet_flavour)!=4&&Mj_isBtag==1_LOOP.eps").c_str());
 
 
   h3->Draw();
   h3->GetXaxis()->SetTitle("Jet_SV_multi");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Jet_SV_multi_FatJetInfo.nJet>0&&abs(FatJetInfo.Jet_flavour)!=5&&abs(FatJetInfo.Jet_flavour)!=4_LOOP.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Jet_SV_multi_FatJetInfo.nJet>0&&abs(FatJetInfo.Jet_flavour)!=5&&abs(FatJetInfo.Jet_flavour)!=4_LOOP.eps").c_str());
 
   h4->Draw();
   h4->GetXaxis()->SetTitle("Jet_SV_multi");
 
-  c1->SaveAs(("rootfiles/"+fname+"_Jet_SV_multi_FatJetInfo.nJet>0&&abs(FatJetInfo.Jet_flavour)!=5&&abs(FatJetInfo.Jet_flavour)!=4&&Jet_SV_multi>0_LOOP.eps").c_str());
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_Jet_SV_multi_FatJetInfo.nJet>0&&abs(FatJetInfo.Jet_flavour)!=5&&abs(FatJetInfo.Jet_flavour)!=4&&Jet_SV_multi>0_LOOP.eps").c_str());
 
-  //
+  // // - pt dependence not per bin
+
+  // float v1[nbins];
+  // float v1_std[nbins];
+  // float v2[nbins];
+  // float v2_std[nbins];
+  // float x[nbins];
+  // float Y[nbins];
+
+  // for (int i =0 ; i<nbins;i++){
+  //   //v1[i] = h1->Integral(i+1,nbins+1); // WHICH DENOMINATOR??
+  //   v1[i] = h1->Integral(1,nbins+1); // WHICH DENOMINATOR??
+  //   v1_std[i] = sqrt(v1[i]);
+  //   v2[i] = h2->Integral(i+1,nbins+1);
+  //   v2_std[i] = sqrt(v2[i]);
+  //   Y[i] = (v2[i]==0? 0 : v2[i]/v1[i]);
+  //   x[i] = xmin + i * (xmax-xmin)/nbins;
+  // }
+
+  // TGraph *gr = new TGraph(nbins,x,Y);
+  // gr->Draw("AP*L");
+  // gr->SetTitle("ratio of #frac{IVF matched, non-b/c Mj}{non-b/c Mj}");
+  // gr->GetXaxis()->SetTitle("p_{T}");
+
+  // c1->SetGridy();
+  // c1->SaveAs(("rootfiles/"+fname+postfix+"_MjBtagfakerate_PtDependance.eps").c_str());
+
+  // - pt dependence per bin
 
   float v1[nbins];
   float v1_std[nbins];
   float v2[nbins];
   float v2_std[nbins];
   float x[nbins];
-  float Y[nbins];
+  float ratio_[nbins];
+  float r_err_x[nbins];
+  float r_err_y[nbins];
 
   for (int i =0 ; i<nbins;i++){
-    v1[i] = h1->Integral(i+1,nbins+1);
+    v1[i] = h1->GetBinContent(i+1);
     v1_std[i] = sqrt(v1[i]);
-    v2[i] = h2->Integral(i+1,nbins+1);
+    v2[i] = h2->GetBinContent(i+1);
     v2_std[i] = sqrt(v2[i]);
-    Y[i] = (v2[i]==0? 0 : v2[i]/v1[i]);
-    x[i] = xmin + i * (xmax-xmin)/nbins;
+    ratio_[i] = (v2[i]==0? 0 : v2[i]/v1[i]);
+    r_err_x[i] = (xmax - xmin)/nbins *0.5;
+    r_err_y[i] = ratio_[i] * sqrt( (v1_std[i] / v1[i])*(v1_std[i] / v1[i])  + (v2_std[i] / v2[i])*(v2_std[i] / v2[i]) ) ;
+    x[i] = xmin + i * (xmax-xmin)/nbins ;
   }
 
-  TGraph *gr = new TGraph(nbins,x,Y);
-  gr->Draw("AP*L");
-  gr->SetTitle("ratio of #frac{IVF matched, non-b/c Mj}{non-b/c Mj}");
+  TGraphErrors *gr = new TGraphErrors(nbins,x,ratio_,r_err_x,r_err_y);
+  gr->Draw("AP");
+  gr->SetTitle("ratio of #frac{IVF matched non-b/c Mj}{non-b/c Mj}");
   gr->GetXaxis()->SetTitle("p_{T}");
 
   c1->SetGridy();
-  c1->SaveAs(("rootfiles/"+fname+"_IVFfakerate_PtDependance.eps").c_str());
-
+  c1->SaveAs(("rootfiles/"+fname+postfix+"_MjBtagfakerate_PtDependance_binbybin.eps").c_str());
 
 }
 //  LocalWords:  TFile
